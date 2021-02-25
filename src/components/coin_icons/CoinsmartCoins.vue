880280fdaf34ad2a3699f2905c36f910e0297161
Chris <chris.kieran@gmail.com>
2021-02-24 21:56:31 -0500

    ready for second deploy

---
+++ src/components/coin_icons/CoinsmartCoins.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img src="/../assets/bitcoin.svg" alt="bitcoin" class="icon">
+        <img src="/../assets/ethereum.png" alt="ethereum" class="icon">
+        <img src="/../assets/ripple.svg" alt="ethereum" class="icon">
+        <img src="/../assets/stellar2.png" alt="stellar" class="icon">
+        <img src="/../assets/eos.png" alt="eos" class="icon">
+        <img src="/../assets/neo.png" alt="neo" class="icon">
+        <img src="/../assets/cardano.png" alt="cardano" class="icon">
+        <img src="/../assets/qcad.png" alt="qcad" class="icon">
+        <img src="/../assets/litecoin.png" alt="litecoin" class="icon">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+    width: $coin_icon_width;
+    margin: $coin_icon_margin;
+}
+</style>
---
+++ src/components/coin_icons/NdaxCoins.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img src="/../assets/bitcoin.svg" alt="bitcoin" class="icon">
+        <img src="/../assets/ethereum.png" alt="ethereum" class="icon">
+        <img src="/../assets/ripple.svg" alt="ripple" class="icon">
+        <img src="/../assets/stellar2.png" alt="stellar" class="icon">
+        <img src="/../assets/eos.png" alt="eos" class="icon">
+        <img src="/../assets/cardano.png" alt="cardano" class="icon">
+        <img src="/../assets/litecoin.png" alt="litecoin" class="icon">
+        <img src="/../assets/tether.png" alt="tether" class="icon">
+        <img src="/../assets/polkadot.svg" alt="polkadot" class="icon">
+        <img src="/../assets/link.png" alt="link" class="icon">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+    width: $coin_icon_width;
+    margin: $coin_icon_margin;
+}
+</style>
