a20f92511beb9ed193605b0f243de9cb91ca3101
Chris <chris.kieran@gmail.com>
2021-03-07 13:27:34 -0500

    components rendering but not active state yet

--- src/components/mobile/MobileGrid.vue
+++ src/components/mobile/MobileGrid.vue
@@ -1,47 +1,58 @@
 <template>
     <div class="mobile-grid">
       <p class="description">{{ $t('message.brand.description') }}</p>
-      <mobile-nav v-on:menu-selection="renderView"/>
-      <component :is="componentName" />
+      <div class="nav-container">
+        <menu-item  name="platforms" @click.native="renderView('platforms')" />
+        <menu-item  name="prices" @click.native="renderView('prices')" />
+        <menu-item  name="contact" @click.native="renderView('contact')" />
+      </div>
+      <component :is="componentName"/>
     </div>
 </template>
 
 <script>
-import PlatformCards from '../PlatformCards.vue';
-import MobileNav from './MobileNav.vue';
+import Platforms from '../PlatformCards.vue';
 import Contact from '../../views/Contact.vue';
-import MobilePrices from './MobilePrices.vue';
+import Prices from '../Prices.vue';
+import MenuItem from './MobileMenuItem.vue';
 
 export default {
   components: {
-    platforms: PlatformCards,
-    contact: Contact,
-    prices: MobilePrices,
-    MobileNav,
+    Platforms,
+    Contact,
+    Prices,
+    MenuItem,
   },
   data() {
     return {
       componentName: 'platforms',
+      counter: 0,
     };
   },
   methods: {
-    renderView(value) {
-      this.componentName = value;
+    renderView(component) {
+      this.componentName = component;
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
-.description {
+.mobile-grid {
+  background-color: $darkest;
+  padding-top: 10px;
+  .description {
   color: $lightest;
   text-align: center;
-  padding: 5px;
-  font-weight: bold;
-}
-.mobile-grid {
-  background: $dark;
-  padding-top: 20px;
+  padding: 0px 10px;
+  }
+  .nav-container {
+    background: $darkest;
+    display: flex;
+    align-items: stretch;
+    font-family: 'Prompt', sans-serif;
+    color: $lightest;
+  }
 }
 
 </style>
---
+++ src/components/mobile/MobileMenuItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="nav-item">{{ name }}</div>
+</template>
+
+<script>
+export default {
+  props: ['name'],
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-item {
+    padding: .8em;
+    flex: 1;
+    text-align: center;
+    background: $medium;
+    color: $darkest;
+    p {
+        margin: .2rem auto;
+    }
+}
+.active {
+  font-size: 30px;
+}
+</style>
