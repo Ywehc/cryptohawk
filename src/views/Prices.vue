4b7bccf256a55187af647e2fdc4be19a743a5f1e
Chris <chris.kieran@gmail.com>
2021-03-13 14:32:56 -0500

    attempt api loading fix

--- src/views/Prices.vue
+++ src/views/Prices.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="sidebar">
-      <p class="text-center p-3 heading">Current prices in $CAD</p>
+      <div class="loaded" v-if="apiLoading === false">
+        <p class="text-center p-3 heading">Current prices in $CAD</p>
       <table class="table table-striped">
         <tbody>
           <PricesRow
@@ -95,6 +96,8 @@
       <p class="nomics text-center">
         <a href="https://nomics.com">{{ $t('message.nomics') }}</a>
       </p>
+      </div>
+      <div v-else class="loading text-center m-4"><p>loading prices...</p></div>
     </div>
 </template>
 
@@ -107,6 +110,7 @@ export default {
   data() {
     return {
       currencies: [],
+      apiLoading: true,
     };
   },
   mounted() {
@@ -114,6 +118,7 @@ export default {
       .get(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.VUE_APP_NOMICS}&ids=BTC,ETH,XRP,ADA,USDT,BNB,DOT,LTC,LINK,USDC,XLM,BCH,UNI,DOGE&interval=1d,30d&convert=CAD&per-page=100&page=1`)
       // eslint-disable-next-line no-return-assign
       .then((response) => this.currencies = response)
+      .then(this.apiLoading = false)
       .catch((error) => { console.log(error.response); });
   },
 };
